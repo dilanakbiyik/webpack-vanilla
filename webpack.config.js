0a9c9d17b2038f1e27992475ec8f0545159970e2
dilanakbiyik <dilanyavuz.93@gmail.com>
2019-10-22 21:47:28 +0200

    Solve dev issue Add readme file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/accessories.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
